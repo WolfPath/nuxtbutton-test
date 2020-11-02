2905137e8ff63a35f24b475eabb0ad8c912f6c1c
Wolfgang <wolfiherrmann@gmail.com>
2020-11-02 17:03:46 +0100

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <form method="POST" ref="mpBtn" id="mpBtn">
+    <script
+      src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+      data-preference-id="654853549-4154ee41-3993-432a-a273-a45a9c23c559"
+      data-header-color="#e8e6d6"
+      data-elements-color="#e8e6d6"
+      data-button-label="PAGAR"
+    ></script>
+  </form>
+</template>
+
+<script>
+export default {};
+</script>
