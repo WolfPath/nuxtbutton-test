dcb65cb1d59035c8d20e58cde0e6fa6e97c23dea
Wolfgang <wolfiherrmann@gmail.com>
2020-11-02 17:56:25 +0100

    second commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,17 @@
 <template>
-  <form method="POST" ref="mpBtn" id="mpBtn">
-    <script
-      src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
-      data-preference-id="654853549-4154ee41-3993-432a-a273-a45a9c23c559"
-      data-header-color="#e8e6d6"
-      data-elements-color="#e8e6d6"
-      data-button-label="PAGAR"
-    ></script>
-  </form>
+  <div>
+    <h1>TEST MP BUTTON (NUXT)</h1>
+    <form method="POST" ref="mpBtn" id="mpBtn">
+      <script
+        src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+        data-preference-id="654853549-4154ee41-3993-432a-a273-a45a9c23c559"
+        data-header-color="#e8e6d6"
+        data-elements-color="#e8e6d6"
+        data-button-label="PAGAR"
+      ></script>
+    </form>
+    <h2>^^^Button Should be Here^^^</h2>
+  </div>
 </template>
 
 <script>
