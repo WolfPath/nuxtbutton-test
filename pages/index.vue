7e46c3266e8e1cbfc7f5b5349b9b89831619e58f
Wolfgang <wolfiherrmann@gmail.com>
2020-11-08 23:21:47 +0100

    life-cycle tests

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <h1>TEST MP BUTTON (NUXT)</h1>
     <form method="POST" ref="mpBtn" id="mpBtn">
       <script
+        v-if="ishere"
         src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
         data-preference-id="654853549-4154ee41-3993-432a-a273-a45a9c23c559"
         data-header-color="#e8e6d6"
@@ -15,5 +16,23 @@
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      ishere: false
+    };
+  },
+  beforeCreate() {
+    this.ishere = true;
+  },
+  created() {
+    this.ishere = true;
+  },
+  beforeMount() {
+    this.ishere = true;
+  },
+  mounted() {
+    this.ishere = true;
+  }
+};
 </script>
