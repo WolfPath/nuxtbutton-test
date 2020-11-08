47593eb7587ca01785196e4e8e68dc09ca3d2204
Wolfgang <wolfiherrmann@gmail.com>
2020-11-08 23:51:39 +0100

    v-html  test 2

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,6 @@ export default {
   data() {
     return {
       ishere: `<script
-        v-if="ishere"
         src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
         data-preference-id="654853549-4154ee41-3993-432a-a273-a45a9c23c559"
         data-header-color="#e8e6d6"
