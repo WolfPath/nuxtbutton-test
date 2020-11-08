aeacd919d46ae8aebb63e63553b7cf8dbad3b5fe
Wolfgang <wolfiherrmann@gmail.com>
2020-11-08 23:36:29 +0100

    v-html  tests

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,7 @@
 <template>
   <div>
     <h1>TEST MP BUTTON (NUXT)</h1>
-    <form method="POST" ref="mpBtn" id="mpBtn">
-      <script
-        v-if="ishere"
-        src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
-        data-preference-id="654853549-4154ee41-3993-432a-a273-a45a9c23c559"
-        data-header-color="#e8e6d6"
-        data-elements-color="#e8e6d6"
-        data-button-label="PAGAR"
-      ></script>
-    </form>
+    <form method="POST" ref="mpBtn" id="mpBtn" v-html="ishere"></form>
     <h2>^^^Button Should be Here^^^</h2>
   </div>
 </template>
@@ -19,20 +10,15 @@
 export default {
   data() {
     return {
-      ishere: false
+      ishere: `<script
+        v-if="ishere"
+        src="https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js"
+        data-preference-id="654853549-4154ee41-3993-432a-a273-a45a9c23c559"
+        data-header-color="#e8e6d6"
+        data-elements-color="#e8e6d6"
+        data-button-label="PAGAR"
+      ><\/script>`
     };
-  },
-  beforeCreate() {
-    this.ishere = true;
-  },
-  created() {
-    this.ishere = true;
-  },
-  beforeMount() {
-    this.ishere = true;
-  },
-  mounted() {
-    this.ishere = true;
   }
 };
 </script>
